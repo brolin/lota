c0e59562a33a718b3ba3dadf53481fa6df701a17
Brolin <brolin108@gmail.com>
2012-02-10 15:25:18 -0500

    Subidas imágenes y cartografía. Falta thumbnails y contacto

---
+++ public/javascripts/client.js
@@ -0,0 +1,24 @@
+/* Fancy box code */
+
+$(document).ready(function() {
+
+    /* This is basic - uses default settings */
+    
+    $("a#single_image").fancybox();
+    
+    /* Using custom settings */
+    
+    $("a#inline").fancybox({
+	'hideOnContentClick': true
+    });
+
+    /* Apply fancybox to multiple items */
+    $("a.grouped_elements").fancybox({
+	'transitionIn':'elastic',
+	'transitionOut':'elastic',
+	'speedIn':600, 
+	'speedOut':200, 
+	'overlayShow':false
+    });
+    
+});
